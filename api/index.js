404781979da20050f49af75f47d15fca67d26f3d
Alfa Rizqi <106806776+AlfaRiz011@users.noreply.github.com>
2025-01-15 16:57:20 +0700

    tes vercel

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const dotenv = require('dotenv'); 
+const authenticateToken = require('../middlewares/authMiddleware');  
+
+const userRoutes = require('../routes/User');
+const adminRoutes = require('../routes/Admin');
+const postRoutes = require('../routes/Post');
+const tagsRoutes = require('../routes/Tags');
+const notificationRoutes = require('../routes/Notification');
+const followRoutes = require('../routes/Follow');
+const eventRoutes = require('../routes/Event');
+const registerRoutes = require('../routes/Register');
+const loginRoutes = require('../routes/Login');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+ 
+app.use('/api/register', registerRoutes);
+app.use('/api/login', loginRoutes);
+
+app.use('/api/users', authenticateToken, userRoutes);
+app.use('/api/admins', authenticateToken, adminRoutes);
+app.use('/api/posts', authenticateToken, postRoutes);
+app.use('/api/tags', authenticateToken, tagsRoutes);
+app.use('/api/notifications', authenticateToken, notificationRoutes);
+app.use('/api/follow', authenticateToken, followRoutes);
+app.use('/api/event', authenticateToken, eventRoutes);
+  
+module.exports = app;
